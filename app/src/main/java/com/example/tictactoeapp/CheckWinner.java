207fe4de3d5285c823dc9aa2ba9097e8488808e4
Rijan Chapagain <hrijon.chp@gmail.com>
2020-09-10 14:43:26 +0800

    Updates checkwinner in 5*5 aswell

--- app/src/main/java/com/example/tictactoeapp/CheckWinner.java
+++ app/src/main/java/com/example/tictactoeapp/CheckWinner.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 public class CheckWinner extends AppCompatActivity {
     // check winner
     protected boolean isGameWon(int boardCols, int boardRows, String [][] field) {
-
+        if(boardCols == 3){
             // compare fields to eachother in columns
             for (int i = 0; i < boardCols; i++) {
                 if (field[i][0].equals(field[i][1])
@@ -43,6 +43,51 @@ public class CheckWinner extends AppCompatActivity {
             return false;
         }
 
+        else{
+            // compare fields to eachother in columns
+            for (int i = 0; i < boardCols; i++) {
+                if (field[i][0].equals(field[i][1])
+                        && field[i][0].equals(field[i][2])
+                        && !field[i][0].equals("")
+                        && (field[i][0].equals(field[i][3])
+                        && field[i][0].equals(field[i][4]))
+                ) {
+                    return true;
+                }
+            }
 
+            // compare fields to eachother in rows
+            for (int i = 0; i < boardRows; i++) {
+                if (field[0][i].equals(field[1][i])
+                        && field[0][i].equals(field[2][i])
+                        && !field[0][i].equals("")
+                        && field[0][i].equals(field[3][i])
+                        && field[0][i].equals(field[4][i])
+                ) {
+                    return true;
+                }
+            }
 
+            // Compare diogonal
+            if (field[0][0].equals(field[1][1])
+                    && field[0][0].equals(field[2][2])
+                    && !field[0][0].equals("")
+                    && field[0][0].equals(field[3][3])
+                    && field[0][0].equals(field[4][4])
+            ) {
+                return true;
+            }
+
+            // Compare diogonal
+            if (field[0][4].equals(field[1][3])
+                    && field[0][4].equals(field[2][2])
+                    && !field[0][4].equals("")
+                    && field[0][4].equals(field[3][1])
+                    && field[0][4].equals(field[4][0])
+            ) {
+                return true;
+            }
+                return false;
+        }
+    }
 }
