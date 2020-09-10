f106040eeb1cb89ec9971cfe93577e941fea051e
Rijan Chapagain <hrijon.chp@gmail.com>
2020-09-10 09:28:14 +0800

    Adds handle player class

---
+++ app/src/main/java/com/example/tictactoeapp/HandlePlayer.java
@@ -0,0 +1,13 @@
+package com.example.tictactoeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HandlePlayer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_handle_player);
+    }
+}
